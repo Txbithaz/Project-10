89f0bd494563bb74492cafe1b03b4925ada69b27
Txbithaz <104535601+Txbithaz@users.noreply.github.com>
2022-05-11 18:08:56 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,44 +1,18 @@
-var sea,ship;
-var seaImg,shipImg;
+var hi = [61, 69, 50, 1];
+var bye
 
-function preload(){
+bye = hi[0] + hi[1] + hi[2] + hi[3];
 
-  shipImg1 = loadAnimation("ship-1.png");
-  //shipImg1 = loadAnimation("ship-1.png");
-  //shipImg1 = loadAnimation("ship-1");
-  shipImg1 = loadAnimation("ship-1.png","ship-2.png","ship-1.png","ship-2.png");
-  //shipImg1 = loadAnimation("ship-1","ship-2","ship-1","ship-2");
-  
-  seaImg = loadImage("sea.png");
-}
+console.log (avg = bye/hi.length);
 
-function setup(){
+function setup() 
+{
   createCanvas(400,400);
-  background("blue");
-
-  sea=createSprite(400,200);
-  sea.addImage(seaImg);
-  sea.velocityX = -5;
-  sea.scale=0.3;
-
-  
-  ship = createSprite(130,200,30,30);
-  ship.addAnimation("movingShip",shipImg1);
-  ship.scale =0.25;
-  
 }
 
-function draw() {
-  background(0);
-  sea.velocityX = -3;
+function draw() 
+{
+background(51);
 
-  if(sea.x < 0){
-    //sea.x = 0;
-    //sea.x = sea.width;
-    sea.x = sea.width/8;
-    //sea.y = height;
-  }
-
- 
-  drawSprites();
 }
+
